1ae63041abf20ab6424925656441a26dc2889448
Nikita Arshinov <nikitaarshinov01k@gmail.com>
2025-05-13 15:02:05 +0300

    Initial commit

---
+++ PicPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicPlayground
+//
+//  Created by Никита Аршинов on 13.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicPlayground/PicPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicPlaygroundApp.swift
+//  PicPlayground
+//
+//  Created by Никита Аршинов on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct PicPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
